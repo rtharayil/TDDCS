0c04b33d8988715865945c77b59d935df923a815
Ranjith Tharayil <ranjiththarayil@gmail.com>
2023-11-23 19:25:04 +0530

    added hb

---
+++ base/hbasket/hamaraBasket.Tests/HamaraBasketTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace hamaraBasket
+{
+    [TestFixture]
+    public class HamaraBasketTest
+    {
+        [Test]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            HamaraBasket app = new HamaraBasket(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("fixme", Items[0].Name);
+        }
+    }
+}
